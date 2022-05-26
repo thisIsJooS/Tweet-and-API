2a77b98da3aed31153b20ce8eeec0ffc7bee6573
thisIsJooS <thisis.joos@gmail.com>
2022-05-26 15:05:17 +0900

    unfollow and update profile

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 
 const { isLoggedIn } = require("./middlewares");
 const User = require("../models/user");
@@ -15,6 +16,40 @@ router.post("/:id/follow", isLoggedIn, async (req, res, next) => {
     } else {
       res.status(404).send("no user");
     }
+  } catch (err) {}
+});
+
+router.post("/:id/unfollow", isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+
+    if (user) {
+      await user.removeFollowing(parseInt(req.params.id, 10));
+      res.send("success");
+    } else {
+      res.status(404).send("no user");
+    }
+  } catch (err) {}
+});
+
+router.post("/update", isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (!user) {
+      return res.status(404).send("no user");
+    }
+
+    const check = await bcrypt.compare(req.body.password, user.password);
+    if (!check) {
+      return res.status(404).send("invalid password");
+    }
+
+    await user.set({
+      nick: req.body.nick,
+    });
+    await user.save();
+
+    return res.redirect("/");
   } catch (err) {}
 });
 
